41dcfa10b36d5b8753a3bf1aa181b77c1da6f260
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-06-01 10:35:08 +0200

    added role column to the table migration

--- database/migrations/2020_05_26_080739_add_role_to_users_table.php
+++ database/migrations/2020_05_26_080739_add_role_to_users_table.php
@@ -14,7 +14,7 @@ class AddRoleToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->addColumn('string','role');
+            // $table->addColumn('string','role');
         });
     }
 
@@ -26,7 +26,7 @@ class AddRoleToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
+            // $table->dropColumn('role');
         });
     }
 }
