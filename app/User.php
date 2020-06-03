45ae0574087d406095a5388d7a31cc069edd4615
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-06-03 10:16:23 +0200

    added polymorphic relationship between comments and users

--- app/User.php
+++ app/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     {
         return $this->hasMany('\App\Order');
     }
+    function comments()
+    {
+        return $this->morphMany('\App\Comment','commentable');
+    }
 }
